6423e77eaa94be36c42889566c3b7d695f6d5d98
Youfind4 Inc <louisfriljo@gmail.com>
2021-01-17 23:08:54 +0100

    Youfind4

--- themes/altum/views/admin/partials/footer.php
+++ themes/altum/views/admin/partials/footer.php
@@ -3,5 +3,5 @@
 <footer class="mt-5 d-flex flex-column flex-md-row justify-content-md-between">
     <span class="text-muted"><?= 'Copyright &copy; ' . date('Y') . ' ' . $this->settings->title . '.' ?></span>
 
-    <span class="text-muted">Powered by <img src="<?= SITE_URL . ASSETS_URL_PATH . 'images/altumcode.png' ?>" alt="AltumCode logo" /> <a href="https://altumcode.com/" target="_blank">AltumCode</a>.</span>
+    <span class="text-muted">Powered by <img src="<?= SITE_URL . ASSETS_URL_PATH . 'images/altumcode.png' ?>" alt="AltumCode logo" /> <a href="https://Youfind4.com/" target="_blank">Youfind4</a>.</span>
 </footer>
